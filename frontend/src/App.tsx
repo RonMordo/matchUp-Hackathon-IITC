6de76bd2810c137ce069ec0fde8cd01ec7d45b32
Jakob Lanidao <jakoblaniado@gmail.com>
2025-08-06 20:55:09 +0300

    wip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { RouterProvider } from "react-router";
 import { Router } from "./router/Router";
 import { EventsPage } from "./pages/EventsPage";
+import MyMap from "./pages/EventMapPage";
 
 function App() {
-  return <EventsPage></EventsPage>
+  return <MyMap></MyMap>;
+  //return <EventsPage></EventsPage>
   //<RouterProvider router={Router} />;
 }
 
